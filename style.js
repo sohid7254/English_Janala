440488c0437a9522591274b69265e946e21ced8f
sohid7254 <sohidameen321@gmail.com>
2025-09-04 16:09:34 +0600

    Dynamically added card section on each button according to its info.

--- style.js
+++ style.js
@@ -3,6 +3,36 @@ const loadLessons = () => {
         .then((res) => res.json())
         .then((json) => displayLessons(json.data));
 };
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+    
+    words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+                <h2 class="text-2xl font-bold">${word.word}</h2>
+                <p class="font-semibold">Meaning /Pronounciation</p>
+                <div class="text-2xl font-bold font-Bangla">"${word.meaning}/${word.pronunciation}"</div>
+                <div class="flex justify-between items-center">
+                  <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button> 
+                  <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button> 
+                </div>
+
+            </div>
+        `;
+        wordContainer.append(card);
+    });
+};
+
 const displayLessons = (lessons) => {
     // 1. Get the container & empty
     const levelContainer = document.getElementById("level-container");
@@ -12,12 +42,12 @@ const displayLessons = (lessons) => {
         // 3. creat element
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-                        <button class="btn btn-outline btn-primary">
+                        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
                         <i class="fa-solid fa-book-open"></i>Learn-${lesson.level_no}
                         </button>
         
         `;
-        levelContainer.append(btnDiv)
+        levelContainer.append(btnDiv);
     }
 };
 loadLessons();
